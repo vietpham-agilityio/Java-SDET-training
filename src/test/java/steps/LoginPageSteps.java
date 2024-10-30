4b63c65d7df4cbe8f6559026cba371b85221f06b
Viet Pham <viet.pham@asnet.com.vn>
2024-10-30 15:52:56 +0700

    Implement scenario for navigate login page DEV

---
+++ src/test/java/steps/LoginPageSteps.java
@@ -0,0 +1,25 @@
+package steps;
+
+import actions.AuthenActions;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.serenitybdd.annotations.Steps;
+
+public class LoginPageSteps {
+    @Steps
+    private AuthenActions authenActions;
+
+    @Given("The user is on Dev page")
+    public void the_user_is_on_dev_page() {
+        authenActions.navigateToPage();
+    }
+    @When("The user clicks on the “Log in” button in the navigation bar")
+    public void the_user_clicks_on_the_log_in_button_in_the_navigation_bar() {
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("The user is navigated to the login page.")
+    public void the_user_is_navigated_to_the_login_page() {
+        throw new io.cucumber.java.PendingException();
+    }
+}
---
+++ src/test/resources/features/navigate/navigate_login_page.feature
@@ -0,0 +1,6 @@
+Feature:  Navigate to login page
+
+  Scenario:
+    Given The user is on Dev page
+    When The user clicks on the “Log in” button in the navigation bar
+    Then The user is navigated to the login page.
