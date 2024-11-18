107939478910275bb7620dc71ecad097756e1b81
Viet Pham <viet.pham@asnet.com.vn>
2024-11-18 14:40:20 +0700

    Update SerenityConfigConstants site for testing

--- src/main/java/contants/SerenityConfigConstants.java
+++ src/main/java/contants/SerenityConfigConstants.java
@@ -1,6 +1,5 @@
 package contants;
 
 public interface SerenityConfigConstants {
-    String DEV_URL = "dev.url";
-    String DEV_LOGIN_URL = "dev.login.url";
+    String DASHBOARD_URL = "dashboard.url";
 }
