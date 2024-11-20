abb9546ad7e4f4156c081975b65bc9f37755efae
Viet Pham <viet.pham@asnet.com.vn>
2024-11-20 13:59:49 +0700

    Improve syntax when use string in CELL_TEMPLATE constant

--- src/main/java/pages/EmployeeTableLocatorPage.java
+++ src/main/java/pages/EmployeeTableLocatorPage.java
@@ -3,7 +3,7 @@ package pages;
 import org.openqa.selenium.By;
 
 public class EmployeeTableLocatorPage {
-    private static final String CELL_TEMPLATE = "//tr[@data-index='%d']//td[@data-index='%s']";
+    private static final String CELL_TEMPLATE = "//tr[@data-index='%d']//td[@data-index='%d']";
 
     public By getRowElement(int rowIndex, int cellIndex) {
         return By.xpath(String.format(CELL_TEMPLATE, rowIndex, cellIndex));
